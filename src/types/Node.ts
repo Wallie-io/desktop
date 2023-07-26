704a2b278fcc1cce08384708fd0a95224b36e7b8
Jake Berg <jake@xyzdigital.com>
2023-07-26 11:02:48 -0700

    adds generic node type (not exported yet)

---
+++ src/types/Node.ts
@@ -0,0 +1,19 @@
+export type Node = {
+    key?: string
+    id: string
+    head: string
+    message: string
+    url?: string // comes from reddit posts
+    date: number | string
+    user: string
+    directions: {
+        GunId: string
+    }
+    directionText: string
+    start: string
+    end: string
+    upVotes: number
+    downVotes: number
+    content: string // comes from the original node.blog.post type
+    title?: string // comes from the node.dashboard type
+}
