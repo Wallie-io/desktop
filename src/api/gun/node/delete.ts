70e50652993390c445fa1bcc34db52b8c54442b2
Jake Berg <jake@xyzdigital.com>
2023-07-29 14:22:54 -0700

    add a node specific delete endpoint

---
+++ src/api/gun/node/delete.ts
@@ -0,0 +1,16 @@
+import gun, { namespace } from '@api/gun'
+
+export const deleteNode = (
+    key: string,
+    callback?: (key: string) => void
+): Promise<void> => {
+    return new Promise((resolve) => {
+        gun.get(namespace + '/node')
+            .get(key)
+            .put(null, (awk) => {
+                console.log(`deleted node at key ${key} awk:`, awk)
+                callback && callback(key)
+                resolve()
+            })
+    })
+}
